ff2294768bfd6c2849f090f75af5a22ba1dd10a7
bebix <56645798+bebix@users.noreply.github.com>
2025-03-26 22:02:38 +0200

    initial commit

---
+++ Repository/IMockRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutApp.Models;
+
+namespace WorkoutApp.Repository
+{
+    public interface IMockRepository
+    {
+        IEnumerable<MockModel> GetAll();
+        void Add(MockModel model);
+        void Save();
+    }
+}
---
+++ Repository/MockRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutApp.Models;
+
+
+namespace WorkoutApp.Repository
+{
+    public class MockRepository : IMockRepository
+    {
+        public void Add(MockModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MockModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
