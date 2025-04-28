272195c8ccffc532256537dcc6b07956f63a1708
Mol-Anita <molanita1121@gmail.com>
2025-04-28 10:52:35 +0300

    refact: refactored cart service

--- WorkoutApp/Service/CartService.cs
+++ WorkoutApp/Service/CartService.cs
@@ -1,55 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkoutApp.Repository;
-using WorkoutApp.Models;
+﻿// <copyright file="CartService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WorkoutApp.Service
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using WorkoutApp.Models;
+    using WorkoutApp.Repository;
+
+    /// <summary>
+    /// Provides services for managing cart items and interacting with the cart repository.
+    /// </summary>
     public class CartService
     {
-        CartItemRepository cartItemRepository;
-        ProductRepository productRepository;
+        private readonly CartItemRepository cartItemRepository;
+        private readonly ProductRepository productRepository;
 
-        public CartService(CartItemRepository cartItemRepository, ProductRepository productRepository) {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CartService"/> class.
+        /// </summary>
+        /// <param name="cartItemRepository">The repository for managing cart items.</param>
+        /// <param name="productRepository">The repository for managing products.</param>
+        public CartService(CartItemRepository cartItemRepository, ProductRepository productRepository)
+        {
             this.cartItemRepository = cartItemRepository;
             this.productRepository = productRepository;
         }
 
-        public List<CartItem> GetCartItems()
+        /// <summary>
+        /// Retrieves all cart items asynchronously.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation, containing a list of <see cref="CartItem"/>.</returns>
+        public async Task<List<CartItem>> GetCartItemAsync()
         {
-            return cartItemRepository.GetAll();
+            return (List<CartItem>)await this.cartItemRepository.GetAllAsync();
         }
 
-
-        public CartItem GetCartItemById(int id)
+        /// <summary>
+        /// Retrieves a cart item by its ID asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the cart item to retrieve.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation, containing the <see cref="CartItem"/>.</returns>
+        public async Task<CartItem> GetCartItemByIdAsync(int id)
         {
-            return cartItemRepository.GetItemById(id);
+            return await this.cartItemRepository.GetByIdAsync(id);
         }
 
-        public void IncreaseQuantity(CartItem cartItem)
+        /// <summary>
+        /// Increases the quantity of a specified cart item asynchronously.
+        /// </summary>
+        /// <param name="cartItem">The cart item whose quantity will be increased.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task IncreaseQuantityAsync(CartItem cartItem)
         {
-            cartItemRepository.UpdateById(cartItem.Id, cartItem.Quantity + 1);
+            cartItem.Quantity += 1;
+            await this.cartItemRepository.UpdateAsync(cartItem);
         }
 
-        public void DecreaseQuantity(CartItem cartItem)
+        /// <summary>
+        /// Decreases the quantity of a cart item asynchronously. Deletes it from the cart if the quantity becomes zero.
+        /// </summary>
+        /// <param name="cartItem">The cart item whose quantity will be decreased.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task DecreaseQuantityAsync(CartItem cartItem)
         {
-            if(cartItem.Quantity > 0) 
-                cartItemRepository.UpdateById(cartItem.Id, cartItem.Quantity - 1);
-            else 
-                cartItemRepository.DeleteById(cartItem.Id);
+            if (cartItem.Quantity > 0)
+            {
+                cartItem.Quantity -= 1;
+                await this.cartItemRepository.UpdateAsync(cartItem);
+            }
+            else
+            {
+                await this.cartItemRepository.DeleteAsync(cartItem.Id);
+            }
         }
 
-        public void RemoveCartItem(CartItem cartItem)
+        /// <summary>
+        /// Removes a cart item asynchronously.
+        /// </summary>
+        /// <param name="cartItem">The cart item to be removed from the cart.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task RemoveCartItemAsync(CartItem cartItem)
         {
-            cartItemRepository.DeleteById(cartItem.Id);
+            await this.cartItemRepository.DeleteAsync(cartItem.Id);
         }
 
-        public void AddToCart(int productId, int quantity)
+        /// <summary>
+        /// Adds a product to the cart asynchronously.
+        /// </summary>
+        /// <param name="productId">The ID of the product to be added to the cart.</param>
+        /// <param name="quantity">The quantity of the product to be added.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task AddToCartAsync(int productId, int quantity)
         {
-            cartItemRepository.AddCartItem(productId, quantity);
+            await this.cartItemRepository.CreateAsync(productId, quantity);
         }
     }
 }
