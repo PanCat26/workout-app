46ad19dbf5dc16a955975be6706033c4d3e3d46f
PanCat26 <catalin.pangaleanu@gmail.com>
2025-05-04 16:43:38 +0300

    Added payment page UI.

---
+++ WorkoutApp/View/Pages/PaymentPage.xaml.cs
@@ -0,0 +1,35 @@
+// <copyright file="PaymentPage.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WorkoutApp.View.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Microsoft.UI.Xaml;
+    using Microsoft.UI.Xaml.Controls;
+    using Microsoft.UI.Xaml.Controls.Primitives;
+    using Microsoft.UI.Xaml.Data;
+    using Microsoft.UI.Xaml.Input;
+    using Microsoft.UI.Xaml.Media;
+    using Microsoft.UI.Xaml.Navigation;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PaymentPage : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentPage"/> class.
+        /// </summary>
+        public PaymentPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
