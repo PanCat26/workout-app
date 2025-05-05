29af3193d3f1daaf5eccf0c58e2de1a44a66317a
915-Paulovici-Anndrei <itsfazzeliv2@gmail.com>
2025-05-05 13:03:14 +0300

    Solved warnings from CategoryRepo, CartViewModel, CategoryFilterViewModel

--- WorkoutApp/ViewModel/CategoryFilterViewModel.cs
+++ WorkoutApp/ViewModel/CategoryFilterViewModel.cs
@@ -1,49 +1,79 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using WorkoutApp.Models;
-using WorkoutApp.Service;
+﻿// <copyright file="CategoryFilterViewModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-public class CategoryFilterViewModel : INotifyPropertyChanged
+namespace WorkoutApp.ViewModel
 {
-    private readonly CategoryService categoryService;
-    private Category selectedCategory;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+    using WorkoutApp.Models;
+    using WorkoutApp.Service;
 
-    public ObservableCollection<Category> Categories { get; } = new();
-
-    public Category SelectedCategory
+    /// <summary>
+    /// ViewModel for managing and filtering product categories.
+    /// </summary>
+    public class CategoryFilterViewModel : INotifyPropertyChanged
     {
-        get => selectedCategory;
-        set
+        private readonly CategoryService categoryService;
+
+        private Category? selectedCategory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryFilterViewModel"/> class.
+        /// </summary>
+        /// <param name="categoryService">The category service used to load categories.</param>
+        public CategoryFilterViewModel(CategoryService categoryService)
         {
-            if (selectedCategory != value)
+            this.categoryService = categoryService;
+        }
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Gets the list of available categories.
+        /// </summary>
+        public ObservableCollection<Category> Categories { get; } = new ();
+
+        /// <summary>
+        /// Gets or sets the selected category.
+        /// </summary>
+        public Category? SelectedCategory
+        {
+            get => this.selectedCategory;
+            set
             {
-                selectedCategory = value;
-                OnPropertyChanged();
+                if (this.selectedCategory != value)
+                {
+                    this.selectedCategory = value;
+                    this.OnPropertyChanged();
+                }
             }
         }
-    }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public CategoryFilterViewModel(CategoryService categoryService)
-    {
-        this.categoryService = categoryService;
-    }
-
-    public async Task LoadCategoriesAsync()
-    {
-        var categories = await categoryService.GetAllAsync();
-
-        Categories.Clear();
-        foreach (var category in categories)
+        /// <summary>
+        /// Loads categories asynchronously and populates the Categories collection.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task LoadCategoriesAsync()
         {
-            Categories.Add(category);
-        }
-    }
+            var categories = await this.categoryService.GetAllAsync();
 
-    protected void OnPropertyChanged([CallerMemberName] string name = null) =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            this.Categories.Clear();
+            foreach (var category in categories)
+            {
+                this.Categories.Add(category);
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="name">The name of the property that changed.</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = "") =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
--- WorkoutApp/ViewModel/WishlistViewModel.cs
+++ WorkoutApp/ViewModel/WishlistViewModel.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Threading.Tasks;
-using WorkoutApp.Data.Database;
-using WorkoutApp.Infrastructure.Session;
-using WorkoutApp.Models;
-using WorkoutApp.Repository;
-using WorkoutApp.Service;
+﻿// <copyright file="WishlistViewModel.cs" company="YourCompany">
+// Copyright (c) YourCompany. All rights reserved.
+// </copyright>
 
 namespace WorkoutApp.ViewModel
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Threading.Tasks;
+    using WorkoutApp.Data.Database;
+    using WorkoutApp.Infrastructure.Session;
+    using WorkoutApp.Models;
+    using WorkoutApp.Repository;
+    using WorkoutApp.Service;
+
+    /// <summary>
+    /// ViewModel responsible for managing wishlist operations.
+    /// </summary>
     public class WishlistViewModel
     {
         private readonly IService<WishlistItem> wishlistService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WishlistViewModel"/> class.
+        /// </summary>
         public WishlistViewModel()
         {
             string? connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString;
@@ -21,6 +31,7 @@ namespace WorkoutApp.ViewModel
             {
                 throw new InvalidOperationException("The connection string 'DefaultConnection' is not configured or is null.");
             }
+
             DbConnectionFactory dbConnectionFactory = new DbConnectionFactory(connectionString);
             DbService dbService = new DbService(dbConnectionFactory);
             SessionManager sessionManager = new SessionManager();
@@ -28,23 +39,42 @@ namespace WorkoutApp.ViewModel
             this.wishlistService = new WishlistService(wishlistRepository);
         }
 
+        /// <summary>
+        /// Retrieves all products from the wishlist asynchronously.
+        /// </summary>
+        /// <returns>A collection of wishlist items.</returns>
         public async Task<IEnumerable<WishlistItem>> GetAllProductsFromWishlistAsync()
         {
             IEnumerable<WishlistItem> wishlistItems = await this.wishlistService.GetAllAsync();
             return wishlistItems;
         }
 
+        /// <summary>
+        /// Adds a product to the wishlist.
+        /// </summary>
+        /// <param name="product">The product to add.</param>
+        /// <returns>The created wishlist item.</returns>
         public async Task<WishlistItem> AddProductToWishlist(Product product)
         {
             return await this.wishlistService.CreateAsync(new WishlistItem(null, product, 1));
         }
 
+        /// <summary>
+        /// Removes a product from the wishlist.
+        /// </summary>
+        /// <param name="wishlistItemID">The ID of the wishlist item to remove.</param>
+        /// <returns>True if the item was removed successfully.</returns>
         public async Task<bool> RemoveProductFromWishlist(int wishlistItemID)
         {
             return await this.wishlistService.DeleteAsync(wishlistItemID);
         }
 
-        public async Task<WishlistItem> GetProductFromWishlist(int productId)
+        /// <summary>
+        /// Retrieves a product from the wishlist by product ID.
+        /// </summary>
+        /// <param name="productId">The product ID to find.</param>
+        /// <returns>The wishlist item, or <c>null</c> if not found.</returns>
+        public async Task<WishlistItem?> GetProductFromWishlist(int productId)
         {
             IEnumerable<WishlistItem> wishlistItems = await this.wishlistService.GetAllAsync();
             foreach (WishlistItem item in wishlistItems)
