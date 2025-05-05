c9dd6f53b643c5a2c6093e2704fdff13207c621c
915-Paulovici-Anndrei <itsfazzeliv2@gmail.com>
2025-05-05 15:15:53 +0300

    Solved warnings

--- WorkoutApp/View/Components/CategoryFilter.xaml.cs
+++ WorkoutApp/View/Components/CategoryFilter.xaml.cs
@@ -1,25 +1,35 @@
-using Microsoft.UI.Xaml.Controls;
-using WorkoutApp.Service;
-using WorkoutApp.Repository;
-using WorkoutApp.Data.Database;
-using System.Configuration;
-using WorkoutApp.Models;
-using System;
-using WorkoutApp.ViewModel;
+// <copyright file="CategoryFilter.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace WorkoutApp.View.Components
 {
+    using System;
+    using System.Configuration;
+    using Microsoft.UI.Xaml.Controls;
+    using WorkoutApp.Data.Database;
+    using WorkoutApp.Models;
+    using WorkoutApp.Repository;
+    using WorkoutApp.Service;
+    using WorkoutApp.ViewModel;
+
+    /// <summary>
+    /// A filter component for selecting a product category.
+    /// </summary>
     public sealed partial class CategoryFilter : UserControl
     {
         private readonly CategoryFilterViewModel viewModel;
 
-        public event EventHandler<int> CategoryChanged;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryFilter"/> class.
+        /// </summary>
         public CategoryFilter()
         {
             this.InitializeComponent();
 
-            var connectionFactory = new DbConnectionFactory(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            var connectionFactory = new DbConnectionFactory(ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' is missing."));
+
             var dbService = new DbService(connectionFactory);
             var categoryRepository = new CategoryRepository(dbService);
             var categoryService = new CategoryService(categoryRepository);
@@ -36,17 +46,28 @@ namespace WorkoutApp.View.Components
             };
         }
 
+        /// <summary>
+        /// Occurs when the selected category changes.
+        /// </summary>
+        public event EventHandler<int>? CategoryChanged;
+
+        /// <summary>
+        /// Resets the selected category filter.
+        /// </summary>
+        public void ResetFilter()
+        {
+            this.CategoryComboBox.SelectedIndex = -1;
+        }
+
+        /// <summary>
+        /// Handles selection change in the category combo box.
+        /// </summary>
         private void CategoryListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (sender is ComboBox comboBox && comboBox.SelectedItem is Category selectedCategory && selectedCategory.ID.HasValue)
             {
                 this.CategoryChanged?.Invoke(this, selectedCategory.ID.Value);
             }
-        }
-
-        public void ResetFilter()
-        {
-            this.CategoryComboBox.SelectedIndex = -1;
         }
     }
 }
--- WorkoutApp/View/Components/DeleteProductButton.xaml.cs
+++ WorkoutApp/View/Components/DeleteProductButton.xaml.cs
@@ -1,113 +1,64 @@
-// WorkoutApp.View.Components/DeleteProductButton.xaml.cs
-// <copyright file="DeleteProductButton.xaml.cs" company="WorkoutApp">
-// Copyright (c) WorkoutApp. All rights reserved.
+// <copyright file="DeleteProductButton.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace WorkoutApp.View.Components // Your specified namespace
+namespace WorkoutApp.View.Components
 {
     using System;
-    using System.Diagnostics; // Required for Debug.WriteLine
-    using System.Threading.Tasks; // Required for Task (though not directly awaited here)
-    using Microsoft.UI.Xaml; // Required for DependencyProperty, DependencyPropertyChangedEventArgs
-    using Microsoft.UI.Xaml.Controls; // Required for UserControl, Button
-    // Removed usings for Service, Data.Database, Repository, ConfigurationManager
-    // as the ViewModel is now provided externally
-
-    using WorkoutApp.ViewModel; // Assuming ProductViewModel is here
+    using System.Diagnostics;
+    using Microsoft.UI.Xaml;
+    using Microsoft.UI.Xaml.Controls;
+    using WorkoutApp.ViewModel;
 
     /// <summary>
     /// A custom button component specifically for Delete actions, receiving its ViewModel externally.
     /// </summary>
-    public sealed partial class DeleteProductButton : UserControl // Inherit from UserControl
+    public sealed partial class DeleteProductButton : UserControl
     {
-        // Removed ProductId Dependency Property as we are passing the ViewModel directly
-        /*
-        public static readonly DependencyProperty ProductIdProperty =
-            DependencyProperty.Register(
-                nameof(ProductId),
-                typeof(int),
-                typeof(DeleteProductButton),
-                new PropertyMetadata(0, OnProductIdChanged));
-        */
-
         /// <summary>
         /// Identifies the <see cref="ViewModel"/> DependencyProperty.
         /// This property holds the instance of the ProductViewModel for this button.
-        /// The ViewModel is provided by the parent View.
         /// </summary>
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register(
                 nameof(ViewModel),
                 typeof(ProductViewModel),
                 typeof(DeleteProductButton),
-                new PropertyMetadata(null, OnViewModelPropertyChanged)); // Default value null, call OnViewModelPropertyChanged when changed
-
+                new PropertyMetadata(null, OnViewModelPropertyChanged));
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DeleteProductButton"/> class.
         /// </summary>
         public DeleteProductButton()
         {
-            Debug.WriteLine("DeleteProductButton: Constructor called."); // Added logging
-            this.InitializeComponent(); // Initialize the XAML defined in DeleteProductButton.xaml
-            // Removed Loaded event subscription as ViewModel initialization is external
-            // Loaded += DeleteProductButton_Loaded;
+            Debug.WriteLine("DeleteProductButton: Constructor called.");
+            this.InitializeComponent();
         }
-
-        // Removed ProductId property
-        /*
-        public int ProductId
-        {
-            get => (int)GetValue(ProductIdProperty);
-            set => SetValue(ProductIdProperty, value);
-        }
-        */
 
         /// <summary>
         /// Gets or sets the instance of the ProductViewModel for this button.
-        /// This is a Dependency Property. Setting this property will update the DataContext.
         /// </summary>
-        public ProductViewModel ViewModel
+        public ProductViewModel? ViewModel
         {
-            get => (ProductViewModel)GetValue(ViewModelProperty);
-            set => SetValue(ViewModelProperty, value);
+            get => (ProductViewModel?)this.GetValue(ViewModelProperty);
+            set => this.SetValue(ViewModelProperty, value);
         }
-
-        // Removed OnProductIdChanged as ProductId DP is removed
-        /*
-        private static void OnProductIdChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-             // ... logic removed ...
-        }
-        */
 
         /// <summary>
         /// Handles changes to the <see cref="ViewModel"/> DependencyProperty.
         /// Updates the DataContext of the UserControl when the ViewModel is set.
         /// </summary>
+        /// <param name="d">The dependency object where the property changed.</param>
+        /// <param name="e">The event data for the change.</param>
         private static void OnViewModelPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            Debug.WriteLine($"DeleteProductButton: OnViewModelPropertyChanged called. NewValue is {(e.NewValue == null ? "null" : "not null")}."); // Added logging
+            Debug.WriteLine($"DeleteProductButton: OnViewModelPropertyChanged called. NewValue is {(e.NewValue == null ? "null" : "not null")}.");
+
             if (d is DeleteProductButton button && e.NewValue is ProductViewModel newViewModel)
             {
-                Debug.WriteLine($"DeleteProductButton: ViewModel property set. Updating DataContext."); // Added logging
-                // Set the DataContext of the UserControl to the new ViewModel instance
+                Debug.WriteLine("DeleteProductButton: ViewModel property set. Updating DataContext.");
                 button.DataContext = newViewModel;
             }
         }
-
-        // Removed Loaded event handler as ViewModel initialization is external
-        /*
-        private async void DeleteProductButton_Loaded(object sender, RoutedEventArgs e)
-        {
-            // ... logic removed ...
-        }
-        */
-
-        // The Click event handler is still handled by x:Bind in DeleteProductButton.xaml:
-        // Click="{x:Bind ViewModel.ExecuteDeleteAsync}"
-        // This directly calls the ExecuteDeleteAsync method on the ViewModel instance
-        // that is set as the DataContext of the UserControl via the ViewModel DP.
-        // No separate Click event handler is needed in the code-behind for this pattern.
     }
 }
